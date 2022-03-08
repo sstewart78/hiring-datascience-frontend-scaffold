07f7d9a41d5b272a24b8cb98966090c7afb269fc
sstewart78 <samuel.stewart57@gmail.com>
2022-03-08 11:01:35 -0700

    main page and search component (continued)

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,15 +1,30 @@
-import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
+import tw from 'twin.macro';
+import { Header, Search, Detail, Footer } from '@hiring-datascience-frontend-scaffold/app/ui';
+import { Route, Routes } from 'react-router-dom';
 
-const StyledApp = styled.div`
-  // Your style here
+//import { LOAD_PEOPLE } from '@hiring-datascience-frontend-scaffold/app/queries/queries';
+
+const StyledApp = tw.div`
+  bg-black 
+  text-green-50 
+  px-10
 `;
 
 export function App() {
+
   return (
-    <StyledApp>
-      <NxWelcome title="app" />
-    </StyledApp>
+    <>
+      <Header title="Imperial Computer Network" />
+      <StyledApp>
+          <Routes>
+            <Route path="/people/:name" element={<Detail />} />
+            <Route path="/planets/:name" element={<Detail />} />
+            <Route path="/starships/:name" element={<Detail />} />
+            <Route path="/" element={<Search />} />
+          </Routes>
+      </StyledApp>
+      <Footer text="Imperial software version 78678353.235" />
+    </>
   );
 }
 
--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -3,9 +3,40 @@ import * as ReactDOM from 'react-dom';
 
 import App from './app/app';
 
+import { ApolloClient, InMemoryCache, ApolloProvider, from, HttpLink } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { BrowserRouter } from 'react-router-dom';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      alert(`GraphQL error: ${message}`)
+    );
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({
+    uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+    //fetchOptions: {
+    //  mode: 'no-cors',
+    //},
+  }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
+
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>,
   document.getElementById('root')
 );
